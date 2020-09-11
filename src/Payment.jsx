5118ce8dd0f6aaf1461cf70ae449154d98840fc6
ahnaf22 <austcse14@gmail.com>
2020-09-11 14:12:10 +0600

    payment page design done and stripe added via npm

--- src/Payment.jsx
+++ src/Payment.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import './cssfiles/payment.css'
 import { useStateValue } from './StateProvider';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
+import FlipMove from 'react-flip-move';
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
     const history = useHistory();
 
-    useEffect(() => {
-        if (!user) {
-            history.push('/');
-        }
-    }, [user])
+    // useEffect(() => {
+    //     if (!user) {
+    //         history.push('/');
+    //     }
+    // }, [user])
 
 
     return (
         <div className="payment">
             <div className="payment_container">
+                <h1>Checkout!(<Link to='/checkout'>{basket?.length}items</Link>)</h1>
                 <div className="payment_section">
                     <div className="payment_title">
                         <h3>Delivery Address</h3>
@@ -27,8 +30,34 @@ function Payment() {
                         <p>Bangladesh,Dhaka</p>
                     </div>
                 </div>
-                <div className="payment_section"></div>
-                <div className="payment_section"></div>
+                {/* review items */}
+                <div className="payment_section">
+                    <div className="payment_title">
+                        <h3>review Itmes and delivery</h3>
+                    </div>
+                    <div className="payment_items">
+                        <FlipMove >
+                            {basket.map(item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            ))}
+                        </FlipMove>
+                    </div>
+                </div>
+                {/* payment method */}
+                <div className="payment_section">
+                    <div className="payment_title">
+                        <h3>payment method</h3>
+                    </div>
+                    <div className="payment_details">
+                        {/* stripe works here */}
+                    </div>
+                </div>
             </div>
         </div>
     )
